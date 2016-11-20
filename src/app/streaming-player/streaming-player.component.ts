6c5d9e4bf21e1e7d267e6f57dd88fceeaf8c991b
Raul <elecash@gmail.com>
2016-11-20 20:29:24 +0100

    Add HLS, DASH & VOD interoperability demo

--- src/app/streaming-player/streaming-player.component.ts
+++ src/app/streaming-player/streaming-player.component.ts
@@ -15,8 +15,9 @@ export class StreamingPlayerComponent implements OnInit {
 
     streams:Stream[] = [
         { label: 'VOD', source: 'http://static.videogular.com/assets/videos/videogular.mp4' },
-        { label: 'Multi rate Streaming', source: 'http://dash.edgesuite.net/dash264/TestCases/2a/qualcomm/1/MultiResMPEG2.mpd' },
-        { label: 'Live Streaming', source: 'http://vm2.dashif.org/livesim/testpic_6s/Manifest.mpd' }
+        { label: 'DASH: Multi rate Streaming', source: 'http://dash.edgesuite.net/dash264/TestCases/2a/qualcomm/1/MultiResMPEG2.mpd' },
+        { label: 'DASH: Live Streaming', source: 'http://vm2.dashif.org/livesim/testpic_6s/Manifest.mpd' },
+        { label: 'HLS: Streaming', source: 'http://www.streambox.fr/playlists/x36xhzz/x36xhzz.m3u8' }
     ];
 
     constructor() {
