e6831566c587cf2e5c36716bc807a3699a0abd0d
Raul <elecash@gmail.com>
2016-11-24 10:01:23 +0100

    Add HLS streaming test file

--- src/app/streaming-player/streaming-player.component.ts
+++ src/app/streaming-player/streaming-player.component.ts
@@ -17,7 +17,7 @@ export class StreamingPlayerComponent implements OnInit {
         { label: 'VOD', source: 'http://static.videogular.com/assets/videos/videogular.mp4' },
         { label: 'DASH: Multi rate Streaming', source: 'http://dash.edgesuite.net/dash264/TestCases/2a/qualcomm/1/MultiResMPEG2.mpd' },
         { label: 'DASH: Live Streaming', source: 'http://vm2.dashif.org/livesim/testpic_6s/Manifest.mpd' },
-        { label: 'HLS: Streaming', source: 'http://cdn.atrendee.com/channels/channel1/project1/show1/videos/hls-playlist.m3u8' }
+        { label: 'HLS: Streaming', source: 'http://www.streambox.fr/playlists/x36xhzz/x36xhzz.m3u8' }
     ];
 
     constructor() {
