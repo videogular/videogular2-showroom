0f0fa541cb5e2989a58a7466f3a547568d26b699
Raul <elecash@gmail.com>
2017-04-28 11:38:22 +0200

    Add standalone mode for videogular docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, HostBinding, OnDestroy, OnInit } from "@angular/core";
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+    @HostBinding('class')
+    isStandalone = '';
+
+    subscriptions: Subscription[] = [];
+
+    constructor(private route: ActivatedRoute) {
+
+    }
+
+    ngOnInit() {
+        this.subscriptions.push(
+            this.route.queryParams
+                .subscribe((params: any) => {
+                    if (params.standalone === 'true') {
+                        this.isStandalone = 'is-standalone';
+                    }
+                })
+        );
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach(s => s.unsubscribe());
+    }
 }
