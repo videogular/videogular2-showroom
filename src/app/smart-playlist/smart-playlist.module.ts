24ca263c6da27d9e53d77e8b26782dd373ecadc6
Raul <elecash@gmail.com>
2017-05-05 19:26:30 +0200

    Update demo font. Update to v5.2.6

--- src/app/smart-playlist/smart-playlist.module.ts
+++ src/app/smart-playlist/smart-playlist.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { VgCoreModule } from 'videogular2/core';
 import { VgControlsModule } from 'videogular2/controls';
 import { SmartPlaylistComponent } from './smart-playlist.component';
+import { VgBufferingModule } from 'videogular2/buffering';
 
 @NgModule({
     imports: [
         CommonModule,
         VgCoreModule,
-        VgControlsModule
+        VgControlsModule,
+        VgBufferingModule
     ],
     declarations: [ SmartPlaylistComponent ]
 })
