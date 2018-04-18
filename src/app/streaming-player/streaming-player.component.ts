63ded18a36265f525f1322a46e004f9dbc42d003
Raul <elecash@gmail.com>
2018-04-18 17:58:28 +0200

    Updated to Videogular2 v6.1.1

--- src/app/streaming-player/streaming-player.component.ts
+++ src/app/streaming-player/streaming-player.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from "@angular/core";
-import { VgAPI } from 'videogular2/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { BitrateOption, VgAPI } from 'videogular2/core';
 import { TimerObservable } from 'rxjs/observable/TimerObservable';
 import { Subscription } from 'rxjs';
 import { IDRMLicenseServer } from 'videogular2/streaming';
+import { VgDASH } from 'videogular2/src/streaming/vg-dash/vg-dash';
+import { VgHLS } from 'videogular2/src/streaming/vg-hls/vg-hls';
 
 export interface IMediaStream {
+    type: 'vod' | 'dash' | 'hls';
     source: string;
     label: string;
     token?: string;
@@ -17,14 +20,32 @@ export interface IMediaStream {
     styleUrls: [ './streaming-player.component.css' ]
 })
 export class StreamingPlayerComponent implements OnInit {
+    @ViewChild(VgDASH) vgDash: VgDASH;
+    @ViewChild(VgHLS) vgHls: VgHLS;
+
     currentStream: IMediaStream;
     api: VgAPI;
 
-    streams:IMediaStream[] = [
-        { label: 'VOD', source: 'http://static.videogular.com/assets/videos/videogular.mp4' },
-        { label: 'DASH: Multi rate Streaming', source: 'https://s3.amazonaws.com/_bc_dml/example-content/sintel_dash/sintel_vod.mpd' },
-        { label: 'DASH: Live Streaming', source: 'https://24x7dash-i.akamaihd.net/dash/live/900080/dash-demo/dash.mpd' },
+    bitrates: BitrateOption[];
+
+    streams: IMediaStream[] = [
         {
+            type: 'vod',
+            label: 'VOD',
+            source: 'http://static.videogular.com/assets/videos/videogular.mp4'
+        },
+        {
+            type: 'dash',
+            label: 'DASH: Multi rate Streaming',
+            source: 'http://dash.edgesuite.net/akamai/bbb_30fps/bbb_30fps.mpd'
+        },
+        {
+            type: 'dash',
+            label: 'DASH: Live Streaming',
+            source: 'https://24x7dash-i.akamaihd.net/dash/live/900080/dash-demo/dash.mpd'
+        },
+        {
+            type: 'dash',
             label: 'DASH: DRM with Widevine',
             source: 'https://storage.googleapis.com/shaka-demo-assets/angel-one-widevine/dash.mpd',
             licenseServers: {
@@ -34,6 +55,7 @@ export class StreamingPlayerComponent implements OnInit {
             }
         },
         {
+            type: 'hls',
             label: 'HLS: Streaming',
             source: 'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8'
         }
@@ -42,18 +64,31 @@ export class StreamingPlayerComponent implements OnInit {
     constructor() {
     }
 
-    onPlayerReady(api:VgAPI) {
+    onPlayerReady(api: VgAPI) {
         this.api = api;
     }
 
     ngOnInit() {
-        this.currentStream = this.streams[0];
+        this.currentStream = this.streams[ 0 ];
     }
 
-    onClickStream(stream:IMediaStream) {
-        this.api.pause();
+    setBitrate(option: BitrateOption) {
+        switch (this.currentStream.type) {
+            case 'dash':
+                this.vgDash.setBitrate(option);
+                break;
 
-        let timer:Subscription = TimerObservable.create(0, 10).subscribe(
+            case 'hls':
+                this.vgHls.setBitrate(option);
+                break;
+        }
+    }
+
+    onClickStream(stream: IMediaStream) {
+        this.api.pause();
+        this.bitrates = null;
+
+        let timer: Subscription = TimerObservable.create(0, 10).subscribe(
             () => {
                 this.currentStream = stream;
                 timer.unsubscribe();
